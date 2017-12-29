4c6da361522ec86eb0ce5ece95f3cb4aad288187
atomiks <cc.glows@gmail.com>
2017-12-29 21:27:03 +1100

    Add module builder script

---
+++ scripts/lib-script.js
@@ -0,0 +1,63 @@
+/*
+  Builds the `Snippet` module library.
+*/
+const fs = require('fs-extra');
+const cp = require('child_process');
+const path = require('path');
+const chalk = require('chalk');
+
+const SNIPPETS_PATH = './snippets';
+const TEMP_PATH = './temp';
+const IMPORTS = './imports.js'
+
+const codeRE = /```\s*js([\s\S]*?)```/;
+
+console.time('Lib');
+
+try {
+  const snippets = fs.readdirSync(SNIPPETS_PATH)
+    .sort((a, b) => a.toLowerCase() - b.toLowerCase())
+    // turn it into an object so we can add data to it to be used in a different scope
+    .map(name => ({ name }));
+
+  if (!fs.existsSync(TEMP_PATH)) {
+    fs.mkdirSync(TEMP_PATH);
+  }
+
+  fs.writeFileSync(IMPORTS, '');
+
+  let exportStr = 'export default {';
+
+  for (const snippet of snippets) {
+    const snippetData = fs.readFileSync(path.join(SNIPPETS_PATH, snippet.name), 'utf8')
+    const snippetName = snippet.name.replace('.md', '');
+
+    const nodeOnly = ['readFileLines', 'JSONToFile'];
+
+    if (!nodeOnly.includes(snippetName)) {
+      const importData = fs.readFileSync(IMPORTS);
+      fs.writeFileSync(IMPORTS, importData + `\nimport { ${snippetName} } from './temp/${snippetName}.js'`);
+      exportStr += `${snippetName},`
+
+      fs.writeFileSync(
+        `${TEMP_PATH}/${snippetName}.js`,
+        'export ' + snippetData.match(codeRE)[1].replace('\n', '')
+      );
+    }
+  }
+
+  exportStr += '}';
+
+  const importData = fs.readFileSync(IMPORTS);
+  fs.writeFileSync(IMPORTS, importData + `\n${exportStr}`);
+
+  cp.exec('rollup -c scripts/rollup.js', {}, (err, stdout, stderr) => {
+    fs.removeSync(TEMP_PATH);
+    fs.unlink(IMPORTS);
+    console.log(`${chalk.green('SUCCESS!')} Snippet module built!`);
+    console.timeEnd('Lib');
+  })
+} catch (err) {
+  console.log(`${chalk.red('ERROR!')} During module creation: ${err}`);
+  process.exit(1);
+}
